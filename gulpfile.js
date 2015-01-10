ef0ac556bc62b294c03871d10d8fd314ceff72f8
aorrego <aorrego@xumak.com>
2015-01-10 13:31:48 -0500

    Added gulp-load-plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,26 @@
-var fs = require('graceful-fs');
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
-var sass = require('gulp-sass');
-var prefix = require('gulp-autoprefixer');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var ngAnnotate = require('gulp-ng-annotate');
-var cssmin = require('gulp-minify-css');
+var plugins = require('gulp-load-plugins')({ camelize: true });
 
-/**
- * Handler for errors. Shows the notification in the browser and the console.
- *
- * @param {Object|String} e
- * @returns {boolean}
- */
-var browserSyncErrorHandler = function (e) {
-  var message = null;
-
-  if (typeof e === 'object') message = e.message;
-  if (typeof e === 'string') message = e;
-
-  browserSync.notify(message);
-  console.log(message);
-  return true;
-};
-
-// Browser Sync
+// Main Tasks
 // ---------------------------------------------------
+gulp.task('default', ['watch']);
+gulp.task('watch', ['browser-sync', 'watch-files']);
+gulp.task('build', ['sass', 'scripts']);
+
+
+// Watch
+// ---------------------------------------------------
+gulp.task('watch-files', function () {
+  gulp.watch('_scss/**/*', ['sass']);
+  gulp.watch('scripts/src/**/*.js', ['scripts']);
+  gulp.watch(['*.html', 'scripts/main.js'], ['browser-reload']);
+});
+
+
+// Sub Tasks
+// -----------------------------------
+// Browser Sync
 gulp.task('browser-sync', ['sass', 'scripts'], function () {
   browserSync({
     server: {
@@ -39,63 +33,43 @@ gulp.task('browser-reload', function () {
   browserSync.reload();
 });
 
-// Sass
-// ---------------------------------------------------
 // Takes main.scss, add the prefixes and set the compiled file in the css folder.
 gulp.task('sass', function () {
   gulp.src('_scss/main.scss')
-    .pipe(sass({
-      includePaths: ['scss'],
+    .pipe(plugins.sass({
+      includePaths: ['_scss'],
       onError: browserSyncErrorHandler
     }))
-    .pipe(prefix(['last 3 versions', '> 1%'], { cascade: true }))
+    .pipe(plugins.autoprefixer(['last 3 versions', '> 1%'], { cascade: true }))
     .pipe(gulp.dest('css'))
     .pipe(browserSync.reload({stream: true}));
 });
 
-
-// Compile files from _scss into minified css
-gulp.task('sassmin', function () {
-  gulp.src('_scss/main.scss')
-    .pipe(sass({ includePaths: ['scss'] }))
-    .pipe(prefix(['last 3 versions', '> 1%'], { cascade: true }))
-    .pipe(cssmin())
-    .pipe(gulp.dest('css/'));
-});
-
-
-// Scripts
-// ---------------------------------------------------
 // Concat the scripts in the src folder.
 gulp.task('scripts', function () {
   gulp
     .src(['scripts/src/**/*.js'])
-    .pipe(concat('main.js'))
-    .pipe(ngAnnotate()).on('error', browserSyncErrorHandler)
-    .pipe(gulp.dest('scripts/'));
-});
-
-// Minifies main.js
-gulp.task('uglify', function () {
-  gulp
-    .src(['scripts/src/**/*.js'])
-    .pipe(concat('main.js'))
-    .pipe(uglify())
+    .pipe(plugins.concat('main.js'))
+    .pipe(plugins.ngAnnotate()).on('error', browserSyncErrorHandler)
     .pipe(gulp.dest('scripts/'));
 });
 
 
-// Watch
-// ---------------------------------------------------
-gulp.task('watch', function () {
-  gulp.watch('_scss/**/*', ['sass']);
-  gulp.watch('scripts/src/**/*.js', ['scripts']);
-  gulp.watch(['*.html', 'scripts/main.js'], ['browser-reload']);
-});
+// Utils
+// -----------------------------------
+/**
+ * Handler for errors. Shows the notification in the browser and the console.
+ *
+ * @param {Object|String} e
+ * @returns {boolean}
+ */
+ function browserSyncErrorHandler(e) {
+  var message = null;
 
+  if (typeof e === 'object') message = e.message;
+  if (typeof e === 'string') message = e;
 
-// Tasks
-// ---------------------------------------------------
-gulp.task('default', ['browser-sync', 'watch']);
-gulp.task('build', ['sass', 'scripts']);
-gulp.task('production', ['sassmin', 'uglify']);
+  plugins.browserSync.notify(message);
+  console.log(message);
+  return true;
+}

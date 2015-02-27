68b3649f22b8f1080a4a38d5829d7919433da39c
aorrego <aorrego@xumak.com>
2015-02-27 07:57:21 -0500

    Changed 6to5 for babelify

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var browserify = require('browserify');
-var to5ify = require('6to5ify');
+var babelify = require('babelify');
 var watchify = require('watchify');
 var source = require('vinyl-source-stream');
 var plugins = require('gulp-load-plugins')({ camelize: true });
@@ -85,7 +85,7 @@ function compileScripts(watch) {
   // Use watchify if watch is true and rerun rebundle on update
   if (watch) {
     bundler = watchify(bundler);
-    bundler.transform(to5ify); // 6to5 compiler
+    bundler.transform(babelify); // 6to5 compiler
 
     bundler.on('update', function () {
       rebundle();
